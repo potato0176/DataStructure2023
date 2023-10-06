1638a66ad9163bac67c65d808dfb7cce0fcfd709
kfhwang <kfhwang@nutc.edu.tw>
2023-10-06 15:44:41 +0800

    20231006

--- first.js
+++ first.js
@@ -1,16 +1,22 @@
-var ans=Math.floor(Math.random()*11);
+var ans = Math.floor(Math.random() * 11);
 console.log(ans);
 var readline = require("readline-sync");
 
 // var userName = readline.question('May I have your name? ');
 // console.log('Hi ' + userName + '!');
+do {
+    var guess = readline.question("Please guess a number between 0 and 10?");
+    console.log("Type:"+typeof(guess))
+    //if (isNaN(guess) || guess>10 || guess<0)//is not a number or >10 or  <0
+    if(!isNaN(guess) && guess<=10 && guess>=0)
+        break;
+    else{
+        continue;
+    }    
+}while(true)
 
-var guess = readline.question("Please guess a number between 0 and 10?"); 
-console.log(guess);
-
-
-if(ans === guess){
+if (ans == guess) {
     console.log("U R right!")
-}else{
+} else {
     console.log("Wrong!!!")
 }
