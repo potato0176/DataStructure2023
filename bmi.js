783280961ebbba93fce5e8f34245972a8ced851a
kfhwang <kfhwang@nutc.edu.tw>
2023-10-13 16:02:04 +0800

    20231013

--- bmi.js
+++ bmi.js
@@ -21,4 +21,24 @@ while(true){
 }
 console.log(height);
 console.log(weight);
-bmi?
+//bmi = w/(h^2) w = (18.5~24)*(h^2)
+var bmi = weight/(Math.pow(height/100, 2));
+//var bmi = weight/((height/100)*(height/100));
+console.log(bmi);
+if(bmi<18.5){
+  console.log("喔喔！你需要再吃營養些，讓自己重一些！")
+}else if(bmi<24){
+  console.log("很不錯喔，很標準，請您繼續保持！")
+}else{
+  console.log("肥胖容易引起疾病，您得要多多注意自己的健康囉！")
+  //claasify 24-27, 27-30, 30-35, >35
+  if(bmi<27){
+    console.log("過重：24<=BMI<27");
+  }else if(bmi<30){
+    console.log("輕度肥胖：27 <= BMI < 30");
+  }else if(bmi<35){
+    console.log("中度肥胖：30 <= BMI < 35");
+  }else{
+    console.log("重度肥胖：BMI >= 35");
+  }
+}
